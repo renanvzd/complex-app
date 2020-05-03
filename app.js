75af2c3c9aa7e80bc5b6680f7e4880168ec05fa2
renanvzd <renanvz@outlook.com>
2020-05-03 18:20:47 -0300

    Create View (HTML)

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
 app.get('/', function (req, res) {
-    res.send("Welcome to our new app")
+    res.render('home-guest')
 })
 
 app.listen(3000)
