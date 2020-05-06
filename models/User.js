1b6706c838d87c3670b4c5ec1cbac6655f0844cd
renanvzd <renanvz@outlook.com>
2020-05-06 01:03:59 -0300

    Model - Validations field

--- models/User.js
+++ models/User.js
@@ -1,7 +1,30 @@
+const validator = require("validator")
+
 let User = function(data) {
     this.data = data
+    this.errors = []
 
 }
 
-User.prototype.register = function() {}
+User.prototype.validate = function() {
+    if(this.data.username == "") {this.errors.push("You must provide an username.")}
+    if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
+    //if(this.data.email == "") {this.errors.push("You must provide a valid email adress.")}
+    if(!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email adress.")}
+    if(this.data.password == "") {this.errors.push("You must provide a password.")}
+    if(this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must be at least 12 characters.")}
+    if(this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 charaters.")}
+    if(this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least 12 characters.")}
+    if(this.data.username.length > 30) {this.errors.push("Username cannot exceed 300 charaters.")}
+
+}
+
+User.prototype.register = function() {
+//Step #1: Validate user data
+this.validate()
+
+//Step #2: Only if there are no validation errors
+//Then save our user data into a database
+}    
+
 module.exports = User 
