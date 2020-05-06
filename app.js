125756e53eec3540f4eb878a78ec72f4f71b5d59
renanvzd <renanvz@outlook.com>
2020-05-06 00:21:44 -0300

    Model - First steps

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const router = require('./router')
 
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'ejs')
