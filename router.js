12faf1d1f419635945fa9cd47016636bb220ebea
renanvzd <renanvz@outlook.com>
2020-05-17 18:48:08 -0300

    Create Post and Separate Header in a especific file

--- router.js
+++ router.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('./controllers/userController')
+const postControler = require('./controllers/postController')
+
+//user related routes
 
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
+
+//post related routes
+
+router.get('/create-post', userController.mustBeLoggedIn, postControler.viewCreateScreen)
 
 module.exports = router
