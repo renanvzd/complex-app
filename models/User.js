d1e99db734a8ff7239da8700daa9630dbaf1a1e0
renanvzd <renanvz@outlook.com>
2020-05-22 00:20:51 -0300

    User Profile Screen

--- models/User.js
+++ models/User.js
@@ -93,4 +93,28 @@ User.prototype.getAvatar = function() {
   this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)} ?s=128`     //s=128 -> represents the size of the picture used on avatar
 }
 
+User.findByUsername = function(username) {
+  return new Promise(function(resolve, reject) {
+    if (typeof(username) != "string") {
+      reject()
+      return 
+    }
+    usersCollection.findOne({username: username}).then(function(userDoc) {
+      if (userDoc) {
+        userDoc = new User(userDoc, true)
+        userDoc = {
+          _id: userDoc.data._id,
+          username: userDoc.data.username,
+          avatar: userDoc.avatar
+        }
+        resolve(userDoc)
+      } else {
+        reject()
+      }
+    }).catch(function() {
+      reject()
+    })
+  })
+}
+
 module.exports = User
